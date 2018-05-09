b217f9826b46ba375302731610bf1c800e62d530
Tony Mannino <tonymannino@me.com>
2018-05-09 01:11:45 -0700

    Got authentication working for spotify service

--- server/index.js
+++ server/index.js
@@ -1,126 +1,18 @@
-// "use strict";
-// // Optional. You will see this name in eg. 'ps' or 'top' command
-// process.title = 'node-chat';
-
-// // Port where we'll run the websocket server
-// var webSocketsServerPort = 1337;
-
-// // websocket and http servers
-// var webSocketServer = require('websocket').server;
-// var http = require('http');
-
-// /**
-//  * Global variables
-//  */
-// // latest 100 messages
-// var history = [];
-// // list of currently connected clients (users)
-// var clients = [];
-// /**
-//  * Helper function for escaping input strings
-//  */
-// function htmlEntities(str) {
-//     return String(str)
-//         .replace(/&/g, '&amp;').replace(/</g, '&lt;')
-//         .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-// }
-// // Array with some colors
-// var colors = ['red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange'];
-
-// // ... in random order
-// colors.sort(function (a, b) { return Math.random() > 0.5; });
-// /**
-//  * HTTP server
-//  */
-
-// var server = http.createServer(function (request, response) {
-//     // Not important for us. We're writing WebSocket server,
-//     // not HTTP server
-// });
-
-// server.listen(webSocketsServerPort, function () {
-//     console.log((new Date()) + " Server is listening on port "
-//         + webSocketsServerPort);
-// });
-
-// /**
-//  * WebSocket server
-//  */
-// var wsServer = new webSocketServer({
-//     // WebSocket server is tied to a HTTP server. WebSocket
-//     // request is just an enhanced HTTP request. For more info 
-//     // http://tools.ietf.org/html/rfc6455#page-6
-//     httpServer: server
-// });
-
-// // This callback function is called every time someone
-// // tries to connect to the WebSocket server
-// wsServer.on('request', function (request) {
-//     console.log((new Date()) + ' Connection from origin '
-//         + request.origin + '.');
-//     // accept connection - you should check 'request.origin' to
-//     // make sure that client is connecting from your website
-//     // (http://en.wikipedia.org/wiki/Same_origin_policy)
-//     var connection = request.accept(null, request.origin);
-//     // we need to know client index to remove them on 'close' event
-//     var index = clients.push(connection) - 1;
-//     var userName = false;
-//     var userColor = false;
-//     console.log((new Date()) + ' Connection accepted.');
-//     // send back chat history
-//     if (history.length > 0) {
-//         connection.sendUTF(
-//             JSON.stringify({ type: 'history', data: history }));
-//     }
-//     // user sent some message
-//     connection.on('message', function (message) {
-//         if (message.type === 'utf8') { // accept only text
-//             // first message sent by user is their name
-//             if (userName === false) {
-//                 // remember user name
-//                 userName = htmlEntities(message.utf8Data);
-//                 // get random color and send it back to the user
-//                 userColor = colors.shift();
-//                 connection.sendUTF(
-//                     JSON.stringify({ type: 'color', data: userColor }));
-//                 console.log((new Date()) + ' User is known as: ' + userName
-//                     + ' with ' + userColor + ' color.');
-//             } else { // log and broadcast the message
-//                 console.log((new Date()) + ' Received Message from '
-//                     + userName + ': ' + message.utf8Data);
-
-//                 // we want to keep history of all sent messages
-//                 var obj = {
-//                     time: (new Date()).getTime(),
-//                     text: htmlEntities(message.utf8Data),
-//                     author: userName,
-//                     color: userColor
-//                 };
-//                 history.push(obj);
-//                 history = history.slice(-100);
-//                 // broadcast message to all connected clients
-//                 var json = JSON.stringify({ type: 'message', data: obj });
-//                 for (var i = 0; i < clients.length; i++) {
-//                     clients[i].sendUTF(json);
-//                 }
-//             }
-//         }
-//     });
-//     // user disconnected
-//     connection.on('close', function (connection) {
-//         if (userName !== false && userColor !== false) {
-//             console.log((new Date()) + " Peer "
-//                 + connection.remoteAddress + " disconnected.");
-//             // remove user from the list of connected clients
-//             clients.splice(index, 1);
-//             // push back user's color to be reused by another user
-//             colors.push(userColor);
-//         }
-//     });
-// });
 var app = require('express')();
 var http = require('http').Server(app);
+var request = require('request');
 var io = require('socket.io')(http);
+// var btoa = require('btoa');
+
+var client_id = '';
+var client_secret = '';
+var authMap = {
+  accessToken: "",
+  tokenType: "",
+  expiration: 0
+};
+
+var queue = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -128,6 +20,9 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log("person connected");
+    
+    !authMap.accessToken.length ? authenticate() : console.log('no auth needed');
+
     socket.on('chat message', function(msg){
         console.log("chat message");
     io.emit('chat message', msg);
@@ -137,3 +32,32 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+function streamAudio() {
+
+}
+
+function authenticate() {
+  let credentials = 'YWEyMzFmYTViOWFmNGY4YTg3MzFkM2IyNDRlOTA0ZjI6Y2Q4MTcxOTk4ODhiNGQ2MzkxZDk0MzZmZTczODA4MDM';
+  let url = 'https://accounts.spotify.com/api/token';
+  let options = {
+    url: url,
+    method: 'POST',
+    headers: {
+        'Authorization': `Basic ${credentials}`
+    },
+    json: true,
+    form: {
+      'grant_type':'client_credentials'
+    }
+  };
+
+  request(options, (error, response, body) => {
+    if (!error) {
+      authMap.accessToken = body.access_token;
+      authMap.expiration = body.expires_in;
+      authMap.tokenType = body.tokenType;
+      console.log(body);
+    }
+  });
+}
